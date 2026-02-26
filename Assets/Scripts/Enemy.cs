b4853989273eef5c174a996be15e8fef477bfa77
Grimmshade <noah.turock204@gmail.com>
2026-02-26 12:18:11 -0600

    Update Enemy.cs
    Basic enemy script now functional. This enemy will move towards the player.

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -3,6 +3,10 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     [SerializeField] private GameObject player;
+
+    [SerializeField] private bool flip;
+
+    [SerializeField] private float speed;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -16,9 +20,14 @@ public class Enemy : MonoBehaviour
 
         if (player.transform.position.x > transform.position.x)
         {
-
+            scale.x = Mathf.Abs(scale.x) * -1 * (flip ? -1 : 1);
+            transform.Translate(speed * Time.deltaTime, 0, 0);
+        } else
+        {
+            scale.x = Mathf.Abs(scale.x) * (flip ? -1 : 1);
+            transform.Translate(speed * Time.deltaTime * -1, 0, 0);
         }
 
-        transform.localScale = scale;
+            transform.localScale = scale;
     }
 }
