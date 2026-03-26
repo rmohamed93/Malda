fbd66dd2cd3293d34e4fe8fa8108b42e3ec26d9b
Marluxi <roseric118@gmail.com>
2026-03-26 11:25:52 -0500

    Ignored meta files and fixed deathzone
    Changed deathzone prefab

--- Assets/Scripts/DeathZone.cs
+++ Assets/Scripts/DeathZone.cs
@@ -5,19 +5,8 @@ public class DeathZone : MonoBehaviour
     public GameObject player;
     public Transform respawnPoint;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-    private void onCollisionEnter2D(Collision2D other)
+    private void OnTriggerEnter2D(Collider2D other)
     {
         if(other.gameObject.CompareTag("Player"))
         {
