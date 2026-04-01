79d433db1192babd8e19e55fa61361759d386cfb
rmohamed93 <rmohamed@students.apsu.edu>
2026-04-01 00:09:29 -0500

    Parallax Script
    created a parallax script for backgrounds and crafted for lvl 1

---
+++ Assets/Scripts/Parallax_BG.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class Parallax_BG : MonoBehaviour
+{
+    private float length, startpos;
+    public GameObject camera;
+    public float parallaxEffect;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        startpos = transform.position.x;
+        length = GetComponent<SpriteRenderer>().bounds.size.x;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float temp = (camera.transform.position.x * (1 - parallaxEffect));
+        float distance = (camera.transform.position.x * parallaxEffect);
+        
+        transform.position = 
+        new Vector3(startpos + distance, transform.position.y, transform.position.z);
+        
+        if (temp > startpos + length) startpos += length;
+        else if (temp < startpos - length) startpos -= length;
+    }
+}
