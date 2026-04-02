71690c443867eaf3456e639568ced588c6c7bcd8
Marluxi <roseric118@gmail.com>
2026-04-02 12:18:59 -0500

    Added Running animation
    Added running animation

--- Assets/Scripts/PlayerActions.cs
+++ Assets/Scripts/PlayerActions.cs
@@ -36,6 +36,8 @@ public class PlayerActions : MonoBehaviour
     public Item swordItem;
     private PlayerHealth playerHealth;
 
+    private Animator anim;
+
     void Awake()
     {
         // get rigidbody of player
@@ -54,6 +56,8 @@ public class PlayerActions : MonoBehaviour
 
         // get player health component
         playerHealth = GetComponent<PlayerHealth>();
+
+        anim = GetComponent<Animator>();
 
         // get and initialize the sword on player
         swordItem?.Initialize(gameObject);
@@ -89,6 +93,7 @@ public class PlayerActions : MonoBehaviour
         // Apply horizontal movement
         var currentSpeed = isSprinting ? moveSpeed * sprintMultiplier : moveSpeed;
         rb.linearVelocity = new Vector2(moveInput.x * currentSpeed, rb.linearVelocity.y);
+        anim.SetFloat("Speed", Mathf.Abs(rb.linearVelocity.x));
 
         // Flip
         HandleFlip();
